f35090124d369760defecd81218f67944668ec2e
Tuan Do <tuan.dominh@ivisgroup.com>
2025-04-17 21:16:46 +0700

    init

--- obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc69302ee4855163014fe26d48c76cd83efcc6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
